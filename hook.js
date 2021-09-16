389e771ea12d9eed34b8120a265bbf9d6c572682
marmeden <itsme@eneasmarin.es>
2021-09-16 16:23:15 +0200

    fix

--- hook.js
+++ hook.js
@@ -1,20 +1,9 @@
 var http = require('http')
 
-const url = 'http://185.253.154.183:8888/test'
+const url = 'http://185.253.154.183:8888/login'
 
 http.get(url, (resp) => {
   console.log('heuuu')
-let data = '';
-  // A chunk of data has been received.
-  resp.on('data', (chunk) => {
-    data += chunk;
-  });
-
-  // The whole response has been received. Print out the result.
-  resp.on('end', () => {
-    console.log(JSON.parse(data).explanation);
-  });
-
 }).on("error", (err) => {
   console.log("Error: " + err.message);
 });
